6f421c9a5f728def29fa42e01157c2b2050b8d65
radovansurlak <radovansurlak@gmail.com>
2016-09-27 16:05:48 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>1st Position Google Search Result</title>
+
+  <meta charset="utf-8">
+  <meta name="description" content="You will learn here how to create meta tags and optimize SEO for your website...">
+  <meta name="author" content="">
+
+  <style>
+    @import 'https://fonts.googleapis.com/css?family=Raleway:200,700';
+  </style>
+
+  <link rel="stylesheet" href="css/fonts.css">
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+  <section id="section-intro">
+
+    <header>
+
+      <nav>
+
+        <img id='logo' src="media/logo.png">
+
+
+
+          <ul id="menu">
+            <div id="wrap">
+              <li><a href=#>Benefits</a></li>
+              <li><a href=#>Features</a></li>
+              <li><a href=#>Testimonials</a></li>
+              <input id="buy-now-button" type="button" value="Buy Now"></input>
+            </div>
+          </ul>
+
+
+      </nav>
+
+    </header>
+
+
+    <main>
+<!--
+      <h1>Radovan Surlak</h1>
+      <h2>tricking, photography, psychology</h2>
+      <img src="media/photo.jpg" alt="Portrait of Radovan Surlak">
+
+        <article>
+          <p>
+          </p>
+        </article> -->
+
+    </main>
+  </section>
+
+</body>
+
+<script src="js/scripts.js"></script>
+</html>
